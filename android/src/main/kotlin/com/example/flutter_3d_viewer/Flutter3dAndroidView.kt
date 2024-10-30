f4deb1bbea1c4a04406ddd6a6c2efa2f5ab2d182
zp <zhaopeng0708@icloud.com>
2024-10-31 06:40:46 +0800

    feat: test glb

--- android/src/main/kotlin/com/example/flutter_3d_viewer/Flutter3dAndroidView.kt
+++ android/src/main/kotlin/com/example/flutter_3d_viewer/Flutter3dAndroidView.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.isGone
+import androidx.lifecycle.lifecycleScope
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -17,6 +20,15 @@ import io.github.sceneview.SceneView
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Scale
 import io.github.sceneview.node.ModelNode
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 internal class Flutter3dAndroidView(
     context: Context,
@@ -36,8 +48,6 @@ internal class Flutter3dAndroidView(
     private val sessionManagerChannel: MethodChannel = MethodChannel(messenger, "arsession_$id")
 
     // UI variables
-    private lateinit var arSceneView: View
-
     // private var modelBuilder = ArModelBuilder()
     // Cloud anchor handler
     private lateinit var sceneView: SceneView
@@ -62,7 +72,6 @@ internal class Flutter3dAndroidView(
         }
 
     override fun getView(): View {
-
         return sceneView
     }
 
@@ -81,25 +90,72 @@ internal class Flutter3dAndroidView(
     init {
         Log.d(TAG, "Initializing AndroidARView")
         viewContext = context
+
         sceneView = SceneView(context)
+        sceneView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
 
+        setupLifeCycle(context)
+        sessionManagerChannel.setMethodCallHandler(onSessionMethodCall)
+        downloadFile()
+    }
 
+    private fun downloadFile() {
+        val url = "https://pb-wolf-temp.materia-app.xyz/api/files/6u8j3gk3siljtfg/gow9zpeolj6x1pu/67203d816f308aaf75f050435e9890aa6dfae50f50_3_d_avatar_moOvkDDCM2.glb?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3OTI1NTEyODAsImlkIjoiYTQ1b3Qwbm5xNnV0eDd0IiwidHlwZSI6ImFkbWluIn0.KAyHwgq-Bzj2i5rnYo-ucuEaW5Jxpz2Wa2PJWtCULNg"
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "Download failed: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (!response.isSuccessful) {
+                    Log.e(TAG, "Download failed: Response code ${response.code}")
+                    return
+                }
+
+                val directory = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "MyAppDownloads")
+                if (!directory.exists()) {
+                    directory.mkdirs()
+                }
+
+                val file = File(directory, "downloaded_file.glb")
+
+                displayGlb(file)
+
+//                val responseBody = response.body
+//                val bytes = responseBody?.bytes()
+//
+//                try {
+//                    FileOutputStream(file).use { fos ->
+//                        fos.write(bytes)
+//                    }
+//                    Log.i(TAG, "Download completed and saved to ${file.absolutePath}")
+//                } catch (e: IOException) {
+//                    Log.e(TAG, "Error writing file: ${e.message}")
+//                }
+            }
+        })
+    }
+
+    private fun displayGlb(file: File) {
         sceneView.cameraNode.apply {
             position = Position(z = 4.0f)
         }
-        val modelFile = "models/Astronaut.glb"
+        val modelFile = "models/avatar-v1.glb"
         val modelInstance = sceneView.modelLoader.createModelInstance(modelFile)
 
         val modelNode = ModelNode(
             modelInstance = modelInstance,
             scaleToUnits = 2.0f,
+            centerOrigin = Position(0.0F, 0.0F, 0.0F)
         )
 //        modelNode.scale = Scale(0.05f)
         sceneView.addChildNode(modelNode)
-//            loadingView.isGone = true
-
-        setupLifeCycle(context)
-        sessionManagerChannel.setMethodCallHandler(onSessionMethodCall)
     }
 
     private fun setupLifeCycle(context: Context) {
