9bb22662d46a07305da2dd840ac96c9bef225596
zp <zhaopeng0708@icloud.com>
2024-10-31 10:13:49 +0800

    fix: android error

--- android/src/main/kotlin/com/example/flutter_3d_viewer/Flutter3dAndroidView.kt
+++ android/src/main/kotlin/com/example/flutter_3d_viewer/Flutter3dAndroidView.kt
@@ -3,43 +3,13 @@ package com.example.flutter_3d_viewer
 import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
 import android.util.Log
-import android.view.MotionEvent
-import android.view.PixelCopy
 import android.view.View
-import android.widget.Toast
-import io.flutter.FlutterInjector
-import io.flutter.embedding.engine.loader.FlutterLoader
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-import java.nio.FloatBuffer
-import java.util.concurrent.CompletableFuture
-import androidx.lifecycle.lifecycleScope
-import androidx.core.view.isGone
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import io.github.sceneview.SceneView
-import io.github.sceneview.math.Position
-import io.github.sceneview.math.Rotation
-import io.github.sceneview.math.Scale
-import io.github.sceneview.node.ModelNode
-import kotlinx.coroutines.launch
-
-import android.R
-import android.view.Gravity
-
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 
 internal class Flutter3dAndroidView(
         context: Context,
@@ -60,7 +30,6 @@ internal class Flutter3dAndroidView(
     private lateinit var arSceneView: View
     // private var modelBuilder = ArModelBuilder()
     // Cloud anchor handler
-    private lateinit var sceneView: SceneView
     private lateinit var loadingView: View
 
 
@@ -101,26 +70,26 @@ internal class Flutter3dAndroidView(
         Log.d(TAG, "Initializing AndroidARView")
         viewContext = context
 //        arSceneView = ArSceneView(context)
-        sceneView = SceneView(context)
+//        sceneView = SceneView(context)
 
 //            val hdrFile = "environments/studio_small_09_2k.hdr"
 //            sceneView.environmentLoader.loadHDREnvironment(hdrFile).apply {
 //                sceneView.indirectLight = this?.indirectLight
 //                sceneView.skybox = this?.skybox
 //            }
-            sceneView.cameraNode.apply {
-                position = Position(z = 4.0f)
-            }
-            val modelFile = "models/Astronaut.glb"
-            val modelInstance = sceneView.modelLoader.createModelInstance(modelFile)
-
-            val modelNode = ModelNode(
-                modelInstance = modelInstance,
-                scaleToUnits = 2.0f,
-            )
-            modelNode.scale = Scale(0.05f)
-            sceneView.addChildNode(modelNode)
-            loadingView.isGone = true
+//            sceneView.cameraNode.apply {
+//                position = Position(z = 4.0f)
+//            }
+//            val modelFile = "models/Astronaut.glb"
+//            val modelInstance = sceneView.modelLoader.createModelInstance(modelFile)
+//
+//            val modelNode = ModelNode(
+//                modelInstance = modelInstance,
+//                scaleToUnits = 2.0f,
+//            )
+//            modelNode.scale = Scale(0.05f)
+//            sceneView.addChildNode(modelNode)
+//            loadingView.isGone = true
 
         setupLifeCycle(context)
         sessionManagerChannel.setMethodCallHandler(onSessionMethodCall)
